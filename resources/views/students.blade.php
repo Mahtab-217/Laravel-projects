266d23206999de1a1a20f2a4e6c725a621b06f73
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-12-01 09:51:24 +0430

    new views

---
+++ resources/views/students.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Her name is Ali</h1>
+    
+</body>
+</html>
